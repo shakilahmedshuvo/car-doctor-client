32a791ef8c8c75ca8fc7de21bd6562f661c50bdf
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-13 13:55:27 +0600

    i firebase and set up

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,13 @@
 import img from '../../assets/images/login/login.svg';
 
 const Login = () => {
+
+    const handleLogin = event => {
+        // stop the reload
+        event.preventDefault();
+        
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -13,36 +20,38 @@ const Login = () => {
                             className="text-3xl text-center font-bold">
                             Login
                         </h1>
-                        <div className="form-control">
-                            <label className="label">
-                                <span
-                                    className="label-text">
-                                    Email
-                                </span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span
-                                    className="label-text">
-                                    Password
-                                </span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a
-                                    href="#" className="label-text-alt link link-hover">
-                                    Forgot password?
-                                </a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button
-                                className="btn btn-primary">
-                                Login
-                            </button>
-                        </div>
+                        {/* login form start */}
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span
+                                        className="label-text">
+                                        Email
+                                    </span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span
+                                        className="label-text">
+                                        Password
+                                    </span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a
+                                        href="#" className="label-text-alt link link-hover">
+                                        Forgot password?
+                                    </a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                {/* login submit btn */}
+                                <input className="btn btn-primary" type="submit" value="Login" />
+                            </div>
+                            {/* login form end */}
+                        </form>
                     </div>
                 </div>
             </div>
