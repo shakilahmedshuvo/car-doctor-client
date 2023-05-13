766d217d17808a48c3e409868c1a819f02f780d8
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-13 12:23:05 +0600

    services json file added

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -2,10 +2,21 @@
 
 const Services = () => {
     return (
-        <div>
-            <div>
-                <h3 className="text-3xl text-orange-600">Service</h3>
-                <h2 className="text-5xl font-bold">Our Service Area</h2>
+        <div className="mt-4">
+            <div className="text-center">
+                <h3
+                    className="text-2xl font-bold text-orange-600">
+                    Service
+                </h3>
+                <h2
+                    className="text-5xl font-bold">
+                    Our Service Area
+                </h2>
+                <p>
+                    The majority have suffered alteration in some form, by injected humour, or randomised
+                    <br />
+                    words which donot look even slightly believable.
+                </p>
             </div>
         </div>
     );
