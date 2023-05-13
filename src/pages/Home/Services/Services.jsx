a4c01132a8eb864e9089853fcdf24375a53beb9a
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-13 12:16:46 +0600

    services area section add

---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,14 @@
+
+
+const Services = () => {
+    return (
+        <div>
+            <div>
+                <h3 className="text-3xl text-orange-600">Service</h3>
+                <h2 className="text-5xl font-bold">Our Service Area</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
