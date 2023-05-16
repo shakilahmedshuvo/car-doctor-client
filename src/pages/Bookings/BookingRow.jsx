a5645ab2ad6186231494eedb73cfa036cb9dd6ca
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-05-16 17:00:58 +0600

    add finishing in the booking page

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,15 +1,20 @@
+const BookingRow = ({ booking, handleDelete, handleBookingConfirm }) => {
 
-const BookingRow = ({ booking }) => {
+    const { _id, date, service, price, img, status } = booking;
 
-    const { date, service, price, img } = booking;
+    // 
 
     return (
         <tr>
-            {/* row 1 */}
+            {/* table body section start */}
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button
+                    onClick={() => handleDelete(_id)}
+                    className="btn btn-sm btn-circle">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
             </th>
             <td>
                 <div className="avatar">
@@ -24,10 +29,21 @@ const BookingRow = ({ booking }) => {
             <td>
                 {service}
             </td>
-            <td>{date}</td>
-            <td>${price}</td>
+            <td>
+                {date}
+            </td>
+            <td>
+                ${price}
+            </td>
             <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                {
+                    status === 'confirm' ? <span className="font-bold text-primary">Confirmed</span> :
+                        <button
+                            onClick={() => handleBookingConfirm(_id)}
+                            className="btn btn-ghost btn-xs">
+                            Please Confirm
+                        </button>
+                }
             </th>
         </tr>
     );
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -7,16 +7,61 @@ const Bookings = () => {
     const { user } = useContext(AuthContext);
     const [bookings, setBookings] = useState([]);
 
+    // fetch the data
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
             .then(data => setBookings(data))
-    }, [])
+    }, [url])
+
+    // handle delete function
+    const handleDelete = id => {
+        const proceed = confirm('Are You  Sure You Want To Delete');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('Deleted Successfully')
+                        const remaining = bookings.filter(booking => booking._id !== id);
+                        setBookings(remaining);
+                    }
+                })
+        }
+    }
+
+    // handle Confirm function
+    const handleBookingConfirm = id => {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ status: 'confirm' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.modifiedCount > 0) {
+                    // update state
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id === id);
+                    updated.status = 'confirm'
+                    const newBookings = [updated, ...remaining];
+                    setBookings(newBookings);
+                }
+            })
+    }
 
     return (
         <div>
-            <h2 className="text-5xl"> Your Bookings: {bookings.length}</h2>
+            <h2 className="text-5xl">
+                Your Bookings: {bookings.length}
+            </h2>
             {/* table start */}
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
@@ -25,21 +70,36 @@ const Bookings = () => {
                         <tr>
                             <th>
                                 <label>
-                                    <input type="checkbox" className="checkbox" />
+                                    <input
+                                        type="checkbox"
+                                        className="checkbox" />
                                 </label>
                             </th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
+                            {/* table header start */}
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                Job
+                            </th>
+                            <th>
+                                Favorite Color
+                            </th>
+                            <th>
+
+                            </th>
+                            {/* table header end */}
                         </tr>
                     </thead>
+                    {/* table body */}
                     <tbody>
                         {/* map the data */}
                         {
                             bookings.map(booking => <BookingRow
                                 key={booking._id}
                                 booking={booking}
+                                handleDelete={handleDelete}
+                                handleBookingConfirm={handleBookingConfirm}
                             ></BookingRow>)
                         }
                     </tbody>
